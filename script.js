0b101d67cd18b765b4a39a8b2270dd0f64da72ef
gabrielmz719 <g20998986@gmail.com>
2025-08-13 09:31:24 -0300

    feature:Agora os livros armazenados no array aparecem em um foreach

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const crypto = require("crypto");
-const { prototype } = require("events");
 
+const cardBooks = document.getElementById('cardBooks');
 const myLibrary = [];
 
 function Book(title , author, pages, read) {
@@ -17,9 +16,16 @@ function addBookToLibrary(title , author, pages, read) {
 
 };
 
-
-
+function displayBook(){
+  cardBooks.innerHTML = '';
+   myLibrary.forEach(element =>{
+    let item =  document.createElement('li');
+    item.textContent = `${element.title} - ${element.author} - ${element.pages} páginas - ${element.read}`;
+    cardBooks.appendChild(item);
+  });
+}
 
 addBookToLibrary('teste1','author1','220','não lido');
+addBookToLibrary('teste2','author2','220','não lido');
 
-console.log(myLibrary);
+displayBook();
