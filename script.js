879718b0fa1fbcad9468c99a1a65b54a0c739cf0
gabrielmz719 <g20998986@gmail.com>
2025-08-14 09:58:58 -0300

    feature:adcionei uma caixa dde dialogo que abre um formulario para adcionar livros

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const cardBooks = document.getElementById('cardBooks');
 const myLibrary = [];
 
-function Book(title , author, pages, read) {
+function Book(title, author, pages, read) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
@@ -10,22 +10,45 @@ function Book(title , author, pages, read) {
   this.read = read;
 };
 
-function addBookToLibrary(title , author, pages, read) {
+function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
   return myLibrary.push(new Book(title, author, pages, read));
 
 };
 
-function displayBook(){
+function displayBook() {
   cardBooks.innerHTML = '';
-   myLibrary.forEach(element =>{
-    let item =  document.createElement('li');
+  myLibrary.forEach(element => {
+    let item = document.createElement('li');
     item.textContent = `${element.title} - ${element.author} - ${element.pages} páginas - ${element.read}`;
     cardBooks.appendChild(item);
   });
 }
 
-addBookToLibrary('teste1','author1','220','não lido');
-addBookToLibrary('teste2','author2','220','não lido');
+(function () {
+  var updateButton = document.getElementById("updateDetails");
+  var cancelButton = document.getElementById("cancel");
+  var favDialog = document.getElementById("favDialog");
+
+  // O botão Update abre uma Dialog
+  updateButton.addEventListener("click", function () {
+    favDialog.showModal();
+  });
+
+  // O botão cancelButtom fecha uma Dialog
+  cancelButton.addEventListener("click", function () {
+    favDialog.close();
+  });
+})();
+
+
+addBookToLibrary('O Senhor dos Anéis', 'J.R.R. Tolkien', 1178, 'lido');
+addBookToLibrary('1984', 'George Orwell', 328, 'não lido');
+addBookToLibrary('Harry Potter e a Pedra Filosofal', 'J.K. Rowling', 223, 'lido');
+addBookToLibrary('O Hobbit', 'J.R.R. Tolkien', 310, 'não lido');
+addBookToLibrary('Dom Casmurro', 'Machado de Assis', 256, 'lido');
+addBookToLibrary('O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 96, 'lido');
+
+console.log(myLibrary);
 
 displayBook();
